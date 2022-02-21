2f235371c05175c0a0eb5ec12fb2261f38cdcad3
Rui Santos <rctsantos@parceiro.sonae.pt>
2022-02-21 22:04:18 +0000

    Add validator for StepsBetweenWordsOptions

---
+++ src/BluePrism.Words.Infrastructure/ModelValidators/StepsBetweenWordsOptionsValidator.cs
@@ -0,0 +1,14 @@
+﻿using BluePrism.Words.Domain.Models;
+using FluentValidation;
+
+namespace BluePrism.Words.Infrastructure.ModelValidators;
+
+internal class StepsBetweenWordsOptionsValidator : AbstractValidator<StepsBetweenWordsOptions>
+{
+    public StepsBetweenWordsOptionsValidator()
+    {
+        RuleFor(x => x.Start).NotEmpty().Length(4, 4);
+        RuleFor(x => x.End).NotEmpty().Length(4, 4);
+        RuleFor(x => x.Dictionary).NotEmpty();
+    }
+}
